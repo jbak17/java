21013f59e99ed24bcaa66e4e9561d748e9bcc795
jbak17 <jbak17@student.monash.edu>
2016-06-22 15:40:53 +1000

    assignment complete

--- src/AverageRainfall.java
+++ src/AverageRainfall.java
@@ -4,12 +4,17 @@ public class AverageRainfall {
     public static void main(String[] args){
         //variables
         Scanner keyboard = new Scanner(System.in);
-        int i, year_current = 1;
-        double rainfall = 0.0, months, average;
+        int i, months, year_current = 1;
+        double rainfall = 0.0,  average;
 
         //ask user for number of years
         System.out.println("Please enter the number of years:");
         int years = keyboard.nextInt();
+        if (years < 1){
+        	System.out.println("Number of years less than one. Program will exit...");
+        	System.exit(0);
+        }
+        
         months = years*12;
         //while loop to count years 
     	System.out.println("Enter monthly rainfall (mm):");
@@ -19,6 +24,10 @@ public class AverageRainfall {
         	for (i=1; i<=12; i++){
         		System.out.printf("Year %d month %d: ", year_current, i);
         		double user_input = keyboard.nextDouble();
+        		while (user_input < 0){
+        			System.out.println("Invalid: Enter 0 or greater: ");
+        			user_input = keyboard.nextDouble();
+        		}
         		rainfall += user_input;
         	}
         	
@@ -27,8 +36,8 @@ public class AverageRainfall {
         }
         average = rainfall/months;
         //display outputs
-        System.out.printf("Number of months: %f.0\n", months);
+        System.out.printf("Number of months: %d\n", months);
         System.out.printf("Total rainfall: %.1f centimetres\n", rainfall);
-        System.out.printf("Number of months: %f centimetres\n", (average));
+        System.out.printf("Average rainfall: %f centimetres\n", average);
     }
 }
